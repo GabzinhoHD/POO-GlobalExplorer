7332d1026d29956d347a6f9fcc2424bc418c0338
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2024-09-05 12:07:17 -0300

    Add files via upload

---
+++ modelo/Pessoa.py
@@ -0,0 +1,13 @@
+class Viagem:
+    nome = ""
+    endereço = ""
+    destino = ""
+    ativo = False
+
+pessoa_Vinicius = Viagem()
+pessoa_Luiza = Viagem()
+
+Viagens = [pessoa_Luiza, pessoa_Vinicius]
+
+print(Viagens)
+
