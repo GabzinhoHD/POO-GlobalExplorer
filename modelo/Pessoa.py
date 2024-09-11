09895018c1ece72f0e3be97ffc7774335b450734
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2024-09-11 11:54:15 -0300

    Add files via upload

--- modelo/Pessoa.py
+++ modelo/Pessoa.py
@@ -1,11 +1,15 @@
 class Viagem:
     nome = ""
-    endereço = ""
-    destino = ""
+    pais_Origem = ""
+    pais_Destino = ""
     ativo = False
 
 pessoa_Vinicius = Viagem()
 pessoa_Luiza = Viagem()
+
+pessoa_Luiza.nome = "Luiza Ribeiro"
+pessoa_Luiza.pais_Origem = "Brasil"
+pessoa_Luiza.pais_Destino = "Alemanha"
 
 Viagens = [pessoa_Luiza, pessoa_Vinicius]
 
