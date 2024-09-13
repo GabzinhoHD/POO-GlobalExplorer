3cdcf4c82a76a4b13f68d157a57473048ac2cde8
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2024-09-13 07:47:20 -0300

    Update Pessoa.py

--- modelo/Pessoa.py
+++ modelo/Pessoa.py
@@ -1,20 +1,26 @@
 class Viagem:
+    viagens = []
     def __init__(self, nome, pais_Origem, pais_Destino):
         self.nome = nome
         self.pais_Origem = pais_Origem
         self.pais_Destino = pais_Destino
         self.ativo = False
+        Viagem.viagens.append(self)
 
     def __str__(self):
         return f"{self.nome} | {self.pais_Origem} | {self.pais_Destino} | {self.ativo}"
 
+    def listar_viagens():
+        for viagem in Viagem.viagens:
+            print(f"{viagem.nome} | {viagem.pais_Origem} | {viagem.pais_Destino}")
+
 
 
 pessoa_Vinicius = Viagem("Vinicius", "Brasil", "Canada")
-pessoa_Luiza = Viagem("Luiza Ribeiro", "Brasil", "Alemanha")
+pessoa_Luiza = Viagem("Luiza Ribeiro", "Brasil", "Reino Unido")
+pessoa_Samuel = Viagem("Samuel Wingert", "Alemanha", "Estados Unidos")
+pessoa_Gustavo = Viagem("Gustavo Apolinario", "Argentina", "Italia")
+pessoa_Luanna = Viagem("Luanna Rodrigues", "Brasil", "Venezuela")
+pessoa_Estefan = Viagem("Estefan Xavier", "Brasil", "Japão")
 
-Viagens = [pessoa_Luiza, pessoa_Vinicius]
-
-print(pessoa_Vinicius)
-print(pessoa_Luiza)
-
+Viagem.listar_viagens()
